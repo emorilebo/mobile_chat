1ce9f34cb9ecacd91a5d6607516345c6af2a8132
emorilebo <emorylebo@gmail.com>
2021-10-16 04:27:37 +0100

    project initial bug fix

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,6 +1,28 @@
 import * as React from "react";
-import { Text } from "react-native";
+import { Text, StyleSheet, View, Image } from "react-native";
 
 export default function TabOneScreen() {
-  return <Text style={{ fontSize: 30, color: "red" }}>Hello</Text>;
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{
+          uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png",
+        }}
+        style={styles.image}
+      />
+      <View></View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {},
+  image: {
+    height: 100,
+    width: 100,
+  },
+  text: {
+    fontSize: 30,
+    color: "blue",
+  },
+});
